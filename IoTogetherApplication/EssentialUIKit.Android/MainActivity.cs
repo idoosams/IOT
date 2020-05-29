ff8f151823732ef35f5f30c07b2dae0419e8c095
Eitan Mechtinger <t-eimech@microsoft.com>
2020-05-29 11:19:03 +0300

    Created app + first changes for our usage

---
+++ IoTogetherApplication/EssentialUIKit.Android/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+using Android.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.AndroidSpecific;
+
+namespace EssentialUIKit.Droid
+{
+    [Activity(Label = "Essential UI Kit", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {   
+            Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+
+            base.OnCreate(savedInstanceState);
+
+            Forms.SetFlags("CollectionView_Experimental");
+
+            Forms.Init(this, savedInstanceState);
+
+            Syncfusion.XForms.Android.PopupLayout.SfPopupLayoutRenderer.Init();
+
+            Syncfusion.XForms.Android.Core.Core.Init(this);
+            
+            this.LoadApplication(new App());
+
+            // Change the status bar color
+            this.SetStatusBarColor(Android.Graphics.Color.Argb(255, 0, 0, 0));
+
+            // Enable scrolling to the page when the keyboard is enabled
+            Xamarin.Forms.Application.Current.On<Xamarin.Forms.PlatformConfiguration.Android>().UseWindowSoftInputModeAdjust(WindowSoftInputModeAdjust.Resize);
+        }
+    }
+}
---
+++ IoTogetherApplication/EssentialUIKit/AppLayout/ViewModels/TemplatePageViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Web;
+using System.Xml.Serialization;
+using EssentialUIKit.AppLayout.Models;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace EssentialUIKit.AppLayout.ViewModels
+{
+    [Preserve(AllMembers = true)]
+    [QueryProperty("QueryData", "data1")]
+    public class TemplatePageViewModel : INotifyPropertyChanged
+    {
+        #region Fields
+
+        /// <summary>
+        /// Gets or sets the selected category.
+        /// </summary>
+        private Category selectedCategory;
+
+        #endregion
+
+        #region event
+        /// <summary>
+        /// The declaration of the PropertyChanged event.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged;
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets or sets the selected category.
+        /// </summary>
+        public Category SelectedCategory
+        {
+            get => this.selectedCategory;
+            set
+            {
+                if (TemplatePageViewModel.Equals(value, this.selectedCategory))
+                {
+                    return;
+                }
+
+                this.selectedCategory = value;
+                this.OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// The PropertyChanged event occurs when changing the value of property.
+        /// </summary>
+        /// <param name="propertyName">Property name</param>
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
