edaf8795aefd1be5e7b9ef1145022a8dd5f72084
Roni_Perru <roniperry41@gmail.com>
2020-06-08 18:10:42 +0300

    battery_charge to batteryCahrge

--- IoTogetherApplication/EssentialUIKit/UserStatsTableEntity.cs
+++ IoTogetherApplication/EssentialUIKit/UserStatsTableEntity.cs
@@ -12,15 +12,15 @@ namespace EssentialUIKit
         double latitude; //Geolocation
         double longtitude; //Geolocation
         double speed;//Geolocation
-        double baterry_charge;//Battery
+        double baterryCharge;//Battery
         bool connectivity;//Connectivity
         
-        public UserStatsTableEntity(double latitude, double longtitude, double speed, double battery_charge, bool connectivity)
+        public UserStatsTableEntity(double latitude, double longtitude, double speed, double batteryCharge, bool connectivity)
         {
             this.latitude = latitude;
             this.longtitude = longtitude;
             this.speed = speed;
-            this.baterry_charge = battery_charge;
+            this.baterryCharge = batteryCharge;
             this.connectivity = connectivity;
             
         }
